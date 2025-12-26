8993c47d15519c8de8680632538acde5bb780682
TANTY <trannhattan2412@gmail.com>
2025-12-26 22:36:17 +0700

    Add project files.

---
+++ tnt-wpf-children/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tnt_wpf_children
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseLogin_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void MinimizeLogin_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+    }
+}
