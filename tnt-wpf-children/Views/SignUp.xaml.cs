a41ae369b436107ff43de32ddce2f28f8c6d8ea7
TANTY <trannhattan2412@gmail.com>
2026-01-01 14:06:50 +0700

    tạo form đăng ký, đăng nhập

---
+++ tnt-wpf-children/Views/SignUp.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace tnt_wpf_children.Views
+{
+    /// <summary>
+    /// Interaction logic for SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+        private void CloseSignUp_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+        private void MinimizeSignUp_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            new Login().Show();
+            this.Close();
+        }
+    }
+}
