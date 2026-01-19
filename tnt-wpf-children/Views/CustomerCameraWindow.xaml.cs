4c9b2324da1eaf74b943590fe841e6aa720fb17a
TANTY <trannhattan2412@gmail.com>
2026-01-19 10:19:09 +0700

    tạo màn hình camera quét khuôn mặt

--- tnt-wpf-children/Views/CustomerCameraWindow.xaml.cs
+++ tnt-wpf-children/Views/CustomerCameraWindow.xaml.cs
@@ -5,11 +5,16 @@ namespace tnt_wpf_children.Views
 {
     public partial class CustomerCameraWindow : Window
     {
+        public CameraViewModel ViewModel { get; }
+
         public CustomerCameraWindow(CameraViewModel viewModel)
         {
             InitializeComponent();
+            ViewModel = viewModel;
             CameraPreview.DataContext = viewModel;
         }
+
+        protected override void OnClosed(System.EventArgs e) => base.OnClosed(e);
 
         private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
@@ -18,5 +23,8 @@ namespace tnt_wpf_children.Views
         }
         private void Close_Click(object sender, RoutedEventArgs e) => Close();
         private void Minimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void Scan_Click(object sender, RoutedEventArgs e) => ViewModel.CaptureAndRecognize();
     }
 }
+
